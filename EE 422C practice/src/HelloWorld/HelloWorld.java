d6140d1481ee2670183377037687dac27f1f5599
mina abbassian <mina.abbassian@gmail.com>
2020-08-24 13:37:55 -0500

    Initial commit

---
+++ EE 422C practice/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+package HelloWorld;
+
+public class HelloWorld {
+	
+	
+	public static void fereidoun() {
+		System.out.println("Hello, World!");
+	}
+	
+	public static void printMina() {
+		System.out.println("Mina");
+	}
+	
+	
+}
---
+++ EE 422C practice/src/HelloWorld/Main.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		HelloWorld.fereidoun();
+		
+		HelloWorld.printMina();
+		HelloWorld.printMina();
+	}
+
+}
